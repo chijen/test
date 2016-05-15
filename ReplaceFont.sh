298c7d8a4a70fdac97257b19bf640bd4329cf82b
chijen <chijen_wu@yahoo.com.tw>
2016-05-15 16:18:16 +0800

    Delete ReplaceFont.sh

--- ReplaceFont.sh
+++
@@ -1,5 +0,0 @@
-cd /opt/jivelite/share/jive/fonts
-sudo rm FreeSans.ttf
-sudo rm FreeSansBold.ttf
-sudo ln -s /mnt/mmcblk0p2/tce/optional/ipagp.ttf FreeSans.ttf
-sudo ln -s /mnt/mmcblk0p2/tce/optional/ipagp.ttf FreeSansBold.ttf
