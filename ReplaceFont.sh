4484b569204715c41aec23311dd191e90fcc8546
chijen <chijen_wu@yahoo.com.tw>
2016-05-25 22:01:06 +0800

    Add files via upload

--- ReplaceFont.sh
+++ ReplaceFont.sh
@@ -2,5 +2,5 @@
 cd /opt/jivelite/share/jive/fonts
 sudo rm FreeSans.ttf
 sudo rm FreeSansBold.ttf
-sudo ln -s /mnt/mmcblk0p2/tce/optional/ipagp.ttf FreeSans.ttf
-sudo ln -s /mnt/mmcblk0p2/tce/optional/ipagp.ttf FreeSansBold.ttf
+sudo ln -s /font/ipagp.ttf FreeSans.ttf
+sudo ln -s /font/ipagp.ttf FreeSansBold.ttf
